a74d2b04159b5105dde16492187d52a1402e6bc9
Daniel Oliveros Guerra <donog98@gmail.com>
2022-04-06 13:04:03 -0500

    Added annotations to methods

--- fractionCalculator.py
+++ fractionCalculator.py
@@ -4,13 +4,13 @@ from fractions import Fraction
 operators = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.floordiv}
 
 
-def convert_to_improper(integer, fraction):
+def convert_to_improper(integer: str, fraction: str) -> str:
     numerator, denominator = get_numerator_and_denominator(fraction)
     new_numerator = (int(denominator) * int(integer)) + int(numerator)
     return '{}/{}'.format(str(new_numerator), str(denominator))
 
 
-def get_operands(character):
+def get_operands(character: str):
     mixed_number_first_operand = ''
     improper_fraction_first_operand = ''
     for i in character:
@@ -25,7 +25,7 @@ def get_operands(character):
     return convert_to_improper(mixed_number_first_operand, improper_fraction_first_operand)
 
 
-def calculate(operation: str):
+def calculate(operation: str) -> str:
     split_operation = operation.split()
     first_operand = split_operation[0]
     given_operator = split_operation[1]
@@ -33,7 +33,8 @@ def calculate(operation: str):
     if '_' in first_operand and '_' in second_operand:
         first_improper_operand = get_operands(first_operand)
         second_improper_operand = get_operands(second_operand)
-        final_result = operators[given_operator](float(eval(first_improper_operand)),float(eval(second_improper_operand)))
+        final_result = operators[given_operator](float(eval(first_improper_operand)),
+                                                 float(eval(second_improper_operand)))
         return format_result(str(Fraction(final_result).limit_denominator()))
 
     elif '_' in first_operand and '_' not in second_operand:
@@ -48,15 +49,20 @@ def calculate(operation: str):
                                                  float(eval(second_improper_operand)))
         return format_result(str(Fraction(final_result).limit_denominator()))
 
+    else:
+        final_result = operators[given_operator](float(eval(first_operand)),
+                                                 float(eval(second_operand)))
+        return format_result(str(Fraction(final_result).limit_denominator()))
 
-def get_numerator_and_denominator(given_string):
+
+def get_numerator_and_denominator(fraction: str) -> tuple[str, str]:
     numerator = ''
     denominator = ''
-    for i in given_string:
+    for i in fraction:
         if i == '/':
             break
         numerator += i
-    for j in reversed(given_string):
+    for j in reversed(fraction):
         if j == '/':
             break
         denominator += j
@@ -64,7 +70,7 @@ def get_numerator_and_denominator(given_string):
     return numerator, denominator
 
 
-def format_result(result):
+def format_result(result: str) -> str:
     numerator, denominator = (get_numerator_and_denominator(result))
     if int(numerator) > int(denominator):
         a = int(numerator) // int(denominator)
@@ -74,5 +80,4 @@ def format_result(result):
 
 
 if __name__ == '__main__':
-    print(calculate('1/2 * 3_3/4'))
-    print(calculate('2_3/8 + 9/8'))
+    print(calculate(input('Please enter the operation (without quotation marks): ')))
